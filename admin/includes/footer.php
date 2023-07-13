b7f864b01106544e21d104535f9c8ae434815875
Randika Piyathissa <89309192+Randika00@users.noreply.github.com>
2023-07-13 22:34:33 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 20023 Brought To You By<a href="https://wellprogram.blogspot.com/">Online Zilla</a></strong>
+</footer>
